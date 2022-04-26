078188aa2a986d083e9b4279daba6a45160a63d7
Alina_Borovskih <alinaberseneva@icloud.com>
2022-04-26 21:01:32 +0300

    Add task019

---
+++ Task019/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine ( "Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine());
+int origin = N;
+int rev = 0;
+int dig = 0;
+while( N > 0)
+{
+   dig = N % 10;
+   rev = rev * 10 + dig;
+   N = N /10 ;  
+}  
+if (origin == rev)
+
+Console.WriteLine ($"{origin} является полиндромом");
+
+else 
+
+Console.WriteLine ($"{origin} Не является полиндромом");
